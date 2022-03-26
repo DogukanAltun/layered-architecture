b87adf21767eb5c68e1b221611c18ed7a8d4f5b0
pc <pc@DESKTOP-NR2QRMV>
2022-03-26 20:29:26 +0300

    Proje dosyası ekle.

---
+++ ConsoleIU/Program.cs
@@ -0,0 +1,19 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.InMemory;
+using System;
+
+namespace ConsoleIU
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductManager _productManager = new ProductManager(new InMemoryProductDal());
+
+            foreach (var product in _productManager.GetAll())
+            {
+                Console.WriteLine(product.ProductName);
+            }
+        }
+    }
+}
